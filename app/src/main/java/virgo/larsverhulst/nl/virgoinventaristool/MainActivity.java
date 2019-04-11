133d70f228f33e50acd281b665d4729ac07ff504
L15V <lars.verhulst@ziggo.nl>
2019-04-11 18:51:38 +0200

    Creating project

---
+++ app/src/main/java/virgo/larsverhulst/nl/virgoinventaristool/MainActivity.java
@@ -0,0 +1,13 @@
+package virgo.larsverhulst.nl.virgoinventaristool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
