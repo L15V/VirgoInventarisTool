e035a647626b2ab88350f87a3f4399e9c90a5d3a
Lars Verhulst <lars.verhulst@ziggo.nl>
2019-04-11 19:25:56 +0200

    [add]Main Screen + layout
    Dummy fragment was needed for testing!

--- app/src/main/java/virgo/larsverhulst/nl/virgoinventaristool/MainActivity.java
+++ app/src/main/java/virgo/larsverhulst/nl/virgoinventaristool/MainActivity.java
@@ -1,13 +1,25 @@
 package virgo.larsverhulst.nl.virgoinventaristool;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+
     }
 }
