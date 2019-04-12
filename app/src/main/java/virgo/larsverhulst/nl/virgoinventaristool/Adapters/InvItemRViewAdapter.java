38a4e72ea63f7ae2db6ff8d702903813ab63920b
Lars Verhulst <lars.verhulst@ziggo.nl>
2019-04-12 21:50:52 +0200

    [added]recyclerViewLogic

---
+++ app/src/main/java/virgo/larsverhulst/nl/virgoinventaristool/Adapters/InvItemRViewAdapter.java
@@ -0,0 +1,72 @@
+package virgo.larsverhulst.nl.virgoinventaristool.Adapters;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import virgo.larsverhulst.nl.virgoinventaristool.R;
+import virgo.larsverhulst.nl.virgoinventaristool.Util.InvItem;
+
+public class InvItemRViewAdapter extends RecyclerView.Adapter<InvItemRViewAdapter.InvViewHolder> {
+    private List<InvItem> invItems;
+
+    public static class InvViewHolder extends RecyclerView.ViewHolder{
+        public TextView title;
+        public TextView amountCrates;
+        public TextView amountBottles;
+
+        public View view;
+
+        public InvViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            this.view = itemView.findViewById(R.id.mainScreen_viewPager);
+
+            this.title = itemView.findViewById(R.id.cardView_title);
+            this.amountCrates = itemView.findViewById(R.id.cardView_amountCrates);
+            this.amountBottles = itemView.findViewById(R.id.cardView_amountBottles);
+        }
+    }
+
+    public InvItemRViewAdapter(Application application){
+        this.invItems = new ArrayList<>();
+    }
+
+    @NonNull
+    @Override
+    public InvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item, parent, false);
+
+        return new InvViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InvViewHolder holder, int position){
+        InvItem item = invItems.get(position);
+
+        holder.title.setText(item.getNameOfDrink());
+        holder.amountCrates.setText(Integer.toString(item.getBottles()));
+        holder.amountBottles.setText(Integer.toString(item.getBottles()));
+    }
+
+    @Override
+    public int getItemCount(){
+        return invItems.size();
+    }
+
+    public void update(List<InvItem> items){
+        this.invItems.clear();
+        this.invItems.addAll(items);
+    }
+
+
+}
--- app/src/main/java/virgo/larsverhulst/nl/virgoinventaristool/MainActivity.java
+++ app/src/main/java/virgo/larsverhulst/nl/virgoinventaristool/MainActivity.java
@@ -1,21 +1,50 @@
 package virgo.larsverhulst.nl.virgoinventaristool;
 
+import android.app.Dialog;
 import android.content.pm.ActivityInfo;
 import android.content.res.ColorStateList;
 import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import virgo.larsverhulst.nl.virgoinventaristool.Adapters.InvItemRViewAdapter;
 import virgo.larsverhulst.nl.virgoinventaristool.Adapters.ScreenSlideAdapter;
+import virgo.larsverhulst.nl.virgoinventaristool.Util.InvItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private List<InvItem> items;
+
+    private int cratesToAdd = 0;
+    private int bottlesToAdd = 0;
+
+    private TextView amountCrates;
+    private TextView amountBottles;
+
+    private Button crate1;
+    private Button crate5;
+    private Button crate10;
+    private Button crate20;
+
+    private Button bottle1;
+    private Button bottle5;
+    private Button bottle10;
+    private Button bottle20;
+
+    private Button addButton;
+
 
     /**
      * attributes for the navigation buttons on the main screen
@@ -35,10 +64,12 @@ public class MainActivity extends AppCompatActivity {
     private Button outButton;
 
     /**
-     * attribute for a list with the current added stuff
+     * attributes for a list with the current added stuff
      */
 
     private RecyclerView currentItemsView;
+    private InvItemRViewAdapter invAdapter;
+    private RecyclerView.LayoutManager invLayoutManager;
 
     /**
      * viewpager for holding the fragments
@@ -48,13 +79,14 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Button> navigationButtons;
 
+    Dialog amountPopup;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        /**
-         * fragment implementation methods
-         */
         super.onCreate(savedInstanceState);
 
+        items = new ArrayList<>();
         navigationButtons = new ArrayList<>();
 
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
@@ -70,6 +102,10 @@ public class MainActivity extends AppCompatActivity {
 
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+
+
+        amountPopup = new Dialog(this);
+
 
         /**
          * Assignments to the layout items
@@ -88,6 +124,18 @@ public class MainActivity extends AppCompatActivity {
         navigationButtons.add(merchButton);
         navigationButtons.add(clothesButton);
         navigationButtons.add(otherButton);
+
+        for(int i = 0; i < navigationButtons.size() ; i++){
+            navigationButtons.get(i).setOnClickListener(this);
+        }
+
+        invAdapter = new InvItemRViewAdapter(getApplication());
+        invLayoutManager = new LinearLayoutManager(getApplicationContext());
+        currentItemsView = findViewById(R.id.mainScreen_currentItems);
+
+        currentItemsView.setHasFixedSize(false);
+        currentItemsView.setLayoutManager(invLayoutManager);
+        currentItemsView.setAdapter(invAdapter);
 
         viewPager = findViewById(R.id.mainScreen_viewPager);
         viewPager.setAdapter(new ScreenSlideAdapter(getSupportFragmentManager()));
@@ -117,6 +165,21 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+
+            case R.id.mainScreen_coldDrinkButton:
+                ShowPopup(v);
+                break;
+            case R.id.mainScreen_alcoholButton:
+                items.add(new InvItem("Cola" , 5 ,15));
+                invAdapter.update(items);
+                invAdapter.notifyDataSetChanged();
+                break;
+        }
     }
 
     /**
@@ -154,5 +217,104 @@ public class MainActivity extends AppCompatActivity {
             clothesButton.setBackgroundColor(getResources().getColor(R.color.LightBlue));
             otherButton.setBackgroundColor(getResources().getColor(R.color.LightBlue));
         }
+    }
+
+    public void ShowPopup(View v){
+
+        final TextView amountCrates;
+        final TextView amountBottles;
+
+        Button crate1;
+        Button crate5;
+        Button crate10;
+        Button crate20;
+
+        Button bottle1;
+        Button bottle5;
+        Button bottle10;
+        Button bottle20;
+
+        Button addButton;
+
+        amountPopup.setContentView(R.layout.custom_popup);
+
+
+
+        amountCrates = amountPopup.findViewById(R.id.customPopup_amountOfCrates);
+        amountCrates.setText(Integer.toString(cratesToAdd));
+        amountBottles = amountPopup.findViewById(R.id.customPopup_amountOfBottles);
+        amountBottles.setText(Integer.toString(bottlesToAdd));
+
+        crate1 = amountPopup.findViewById(R.id.customPopup_cr1);
+        crate5 = amountPopup.findViewById(R.id.customPopup_cr5);
+        crate10 = amountPopup.findViewById(R.id.customPopup_cr10);
+        crate20 = amountPopup.findViewById(R.id.customPopup_cr20);
+
+        bottle1 = amountPopup.findViewById(R.id.customPopup_bo1);
+        bottle5 = amountPopup.findViewById(R.id.customPopup_bo5);
+        bottle10 = amountPopup.findViewById(R.id.customPopup_bo10);
+        bottle20 = amountPopup.findViewById(R.id.customPopup_bo20);
+
+        addButton = findViewById(R.id.customPopup_addButton);
+
+        crate1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cratesToAdd += 1;
+                amountCrates.setText(Integer.toString(cratesToAdd));
+            }
+        });
+        crate5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cratesToAdd += 5;
+                amountCrates.setText(Integer.toString(cratesToAdd));
+            }
+        });
+        crate10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cratesToAdd += 10;
+                amountCrates.setText(Integer.toString(cratesToAdd));
+            }
+        });
+        crate20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cratesToAdd += 20;
+                amountCrates.setText(Integer.toString(cratesToAdd));
+            }
+        });
+
+        bottle1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottlesToAdd += 1;
+                amountBottles.setText(Integer.toString(bottlesToAdd));
+            }
+        });
+        bottle5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottlesToAdd += 5;
+                amountBottles.setText(Integer.toString(bottlesToAdd));
+            }
+        });
+        bottle10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottlesToAdd += 10;
+                amountBottles.setText(Integer.toString(bottlesToAdd));
+            }
+        });
+        bottle20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                bottlesToAdd += 20;
+                amountBottles.setText(Integer.toString(bottlesToAdd));
+            }
+        });
+
+        amountPopup.show();
     }
 }
